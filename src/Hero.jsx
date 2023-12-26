1b25bd64a644d7bdd2fc2f16015bde61cf19f6a2
bulascoskun <ulas_coskun99@hotmail.com>
2023-12-26 17:59:56 +0300

    first commit

---
+++ src/Hero.jsx
@@ -0,0 +1,23 @@
+import heroImg from './assets/hero.svg';
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <div className="hero-title">
+          <h1>Contentful CMS</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+            cupiditate recusandae repellendus commodi totam, repellat sit. Ipsum
+            quod atque repellat eaque impedit, cum itaque, ipsa officiis,
+            cupiditate fuga eveniet debitis?
+          </p>
+        </div>
+        <div className="img-container">
+          <img src={heroImg} alt="woman and the browser" className="img" />
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Hero;
